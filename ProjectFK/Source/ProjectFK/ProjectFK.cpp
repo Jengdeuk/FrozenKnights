688c233174225e92ab5d1ce2d214cde646014568
jengdeuk <jengdeuk@gmail.com>
2024-07-08 22:39:41 +0900

    모듈 생성

---
+++ ProjectFK/Source/ProjectFK/ProjectFK.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectFK.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectFK, "ProjectFK" );
---
+++ ProjectFK/Source/ProjectFKGAS/ProjectFKGAS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectFKGAS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_GAME_MODULE( FDefaultGameModuleImpl, ProjectFKGAS );
